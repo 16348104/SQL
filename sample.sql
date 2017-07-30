deb68752ef49f5e1c497ec747c338b03a4db3e8e
16348104 <16348104@qq.com>
2017-07-30 15:24:52 +0800

    update sample.sql

--- sample.sql
+++ sample.sql
@@ -44,4 +44,5 @@ CREATE TABLE salgrade (
 /*Data for the table salgrade */
 insert  into salgrade(GRADE,LOSAL,HISAL) values (1,700,1200),(2,1201,1400),(3,1401,2000),(4,2001,3000),(5,3001,9999);
 
-# SHOW DATABASES;
+SHOW DATABASES;
+# DROP TABLE table_name;
