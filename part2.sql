1a19e7d9a83031dbec925e4e02c97c371f2242d5
16348104 <16348104@qq.com>
2017-11-02 13:42:48 +0800

    update part2

--- part2.sql
+++ part2.sql
@@ -1,11 +1,12 @@
 # PART II
 # 1. 返回拥有员工的部门名、部门号
 SELECT DISTINCT
-  d.DEPTNO,
-  d.DNAME
-FROM scott.dept d
-  JOIN scott.emp e
-    ON d.DEPTNO = e.DEPTNO;
+  emp.DEPTNO,
+  dept.DNAME
+FROM emp,dept
+WHERE emp.DEPTNO = dept.DEPTNO;
+
+
 # 2. 工资多于 scott 的员工信息
 
 # 3. 返回员工和所属经理的姓名
