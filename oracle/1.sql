f98c4a3082a0421a11cad8e369fef8ad3cd8b219
16348104 <16348104@QQ.com>
2018-01-28 17:26:48 +0800

    update part1 oracle

--- oracle/1.sql
+++ oracle/1.sql
@@ -41,33 +41,32 @@ FROM user_tables t INNER JOIN user_tab_comments f ON t.table_name = f.table_name
 WHERE t.table_name LIKE 'BIZ_DICT%';
 -- oracle数据库表名区分大小写
 
+
 --查看当前用户下的所有表
 
 SELECT t.table_name
-FROM USER_TABLES t
-
+FROM USER_TABLES t;
 
 
 --查看表的说明
-
 SELECT *
 FROM user_tab_comments
 WHERE TABLE_NAME = 'USER_INFO';
+
 
 --查看表的相关属性：字段名称,数据类型,是否主键，是否为空，缺省值，是否唯一，字段描述
 SELECT *
 FROM user_tab_columns
 WHERE table_name = 'SYS_USER';
 
---查看表中列的说明
 
+--查看表中列的说明
 SELECT *
 FROM user_col_comments
 WHERE TABLE_NAME = 'USER_INFO';
 
+
 --查询表中的ID列名(cu.*column_name 为ID)
-
-
 SELECT cu.*
 FROM user_cons_columns cu, user_constraints au
 WHERE cu.constraint_name = au.constraint_name AND au.constraint_type = 'P' AND au.table_name = '要查询的表';
