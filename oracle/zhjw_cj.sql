432c51780ab63a7ff86117b2e6cc422bddb314cf
16348104 <16348104@qq.com>
2019-09-18 10:55:07 +0800

    update zhjw

---
+++ oracle/zhjw_cj.sql
@@ -0,0 +1,43 @@
+--更新单个成绩提交状态
+-- cj_bks_bxqcjb
+-- cj_yjs_bxqcjb
+-- cj_jxs_cjb
+-- pt_xkb    LRZTM   001已提交002暂存
+
+-- update cj_bks_bxqcjb
+-- set LRZTM = '002'
+-- where XNXQ = '2013-2014-1'
+--   and KCH = '02070111'
+--   and KXH = '91';
+
+select xnxq,
+       kch,
+       xh,
+       kssj,
+       kxh,
+       jsh1,
+       lrztm,
+       djzcj,
+       fdjzcj,
+       lrsj,
+       CZR
+from cj_bks_bxqcjb
+where XNXQ = '2013-2014-1'
+  and KCH = '02070111'
+  and KXH = '91';
+
+--更新课程成绩全体提交状态
+--CJ_FXB LRZTM  001已提交  002暂存
+
+-- update CJ_FXB
+-- set LRZTM='002'
+-- where XNXQ = '2013-2014-2'
+--   and KCH = '02070111'
+--   and KXH = '91';
+
+
+select XNXQ, KCH, KXH, JSH, LRZTM, CZR, CZSJ
+from CJ_FXB
+where XNXQ = '2013-2014-2'
+  and KCH = '02070111'
+  and KXH = '91';
