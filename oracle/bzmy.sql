e43a00c3f65cfdd7554743b442e97328b3831e74
16348104 <16348104@qq.com>
2020-06-30 14:47:12 +0800

    update zhjw

--- oracle/bzmy.sql
+++ oracle/bzmy.sql
@@ -1,11 +1,11 @@
 -- 重置交费
--- update BZ_MY_KSBMXX
--- set sfyff = '未交',
---     cs_bz = 'Y'
--- where ksbh = '2017000040'
---   and bknf = '2017';
+update BZ_MY_KSBMXX
+set sfyff = '未交',
+    cs_bz = 'Y'
+where ksbh = '2017000040'
+  and bknf = '2017';
 
---重置线上确认状态
+-- 重置线上确认状态
 update BZ_MY_CSB
 set XSQRBZ = '',
     XSQRZT = ''
